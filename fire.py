6627067627f5ba2900435d8bb2fb1b33523c9a7b
Ryan Skeele <rycoske@gmail.com>
2014-08-06 17:26:07 -0700

    Cleaned and commented.

--- fire.py
+++ fire.py
@@ -29,29 +29,7 @@ fli= gdal.Open('data/ash1_raster.fli')
 fli = fli.ReadAsArray()
 toa= toa.ReadAsArray()
 
-class uav():	
-
-	fov_h= 45	#degrees
-	fov_v= 37	#degrees
-	speed_max= 16	#m/s
-	flight_time = 50 #minutes
-	turning_radius= 3 #radius meters
-	start_x= 0
-	start_y= 0
-	pose= 45
-
-uav1 =uav()
-
-
-"""
-Notes
-
-hotspot > 100, or do gradients and find max in gradient then keep track of x gradients
-take in data, use centers to determine if in range
-make movement decision
-move
-"""
-
+# Field of view for UAV is in the following configuration
 view_mask = np.zeros((100, 100))
 rr, cc = polygon(np.array([30, 30, 50]), np.array([40, 60, 50]))
 view_mask[rr, cc] = 1
@@ -61,17 +39,15 @@ fire_map = np.zeros_like(fli)
 im = ax.matshow(fire_map)
 im.set_clim(0, 1000)
 
+#Starting location of UAV in the Fire Map
 uav_pos= (50,50)
 view_mask_rot= view_mask 
-hotspots = np.zeros(10)
- 
-def algorithm(uav_pos,view_mask_rot, hotspots):
-	#hotspots= np.zeros_like(view_mask_rot)
-	hotspots= np.append(np.argsort(-view_mask_rot)[:3], hotspots) #this returns indexs, need to fix
-	hotspots= np.argsort(-hotspots)[:10]
+
+# function for uav location and path planning 
+def uav_pose(uav_pos,view_mask_rot):
 	uav_pos= (uav_pos[0]+1, uav_pos[1]+1)
 	view_mask_rot= rotate(view_mask,uav_pos[0]*5)
-	return uav_pos, view_mask_rot, hotspots
+	return uav_pos, view_mask_rot 
 
 max_time = math.ceil(np.amax(toa))
 
@@ -87,30 +63,33 @@ for t in np.arange(0, max_time, TIMESTEP):
 	frontier = np.where(frontier == True, fire_map, np.zeros_like(fire_map))
 
 	# Position the UAV
-	uav_pos, view_mask_rot, hotspots = algorithm(uav_pos, view_mask_rot, hotspots) #rotate(view_mask, t * 10) #this too
+	uav_pos, view_mask_rot = uav_pose(uav_pos, view_mask_rot) #rotate(view_mask, t * 10) #this too
 	view_mask_rot = view_mask_rot * 100
 
 	(xs, ys) = np.nonzero(frontier)
 	if xs.size > 0:
-		# dists = np.sum((frontier_points - np.array([uav_pos[0], uav_pos[1]])) ** 2, axis=1)
 		dists = (xs - uav_pos[0]) ** 2 + (ys - uav_pos[1]) ** 2
 		nearest = np.argmin(dists)
 
+		# Store the nearest point on fireline 
 		for dx in range(-1, 2):
 			for dy in range(-1, 2):
 				frontier[xs[nearest] + dx, ys[nearest] + dy] = 5000
 
+		# Find the perpendicular vector to the fireline
 		vec_to_nearest = np.array([xs[nearest] - uav_pos[0], ys[nearest] - uav_pos[1]])
 		vec_to_nearest = vec_to_nearest / np.linalg.norm(vec_to_nearest)
 		perp_vec = np.empty_like(vec_to_nearest)
 		perp_vec[0] = -vec_to_nearest[1]
 		perp_vec[1] = vec_to_nearest[0]
 
+		#Plot the perpendicular vector
 		rr, cc = line(uav_pos[0], uav_pos[1],
 			int(uav_pos[0] + perp_vec[0] * 10),
 			int(uav_pos[1] + perp_vec[1] * 10))
 		frontier[rr, cc] = 5000
-
+	
+	# Show the location of the UAV
 	frontier[uav_pos[0], uav_pos[1]] = 5000
 
 	hotspots = np.where(frontier > 500)
